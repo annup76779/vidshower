063154073e36fb1a6431a2eaa559f5f23a3eed71
annup76779 <annup76779@gmail.com>
2023-07-24 23:39:23 +0530

    second commit - offline commit

--- vidshower/settings.py
+++ vidshower/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bi#uzspybh27tgqf*itfjf6(k4las7il=v*mon8(u++vdaf@2f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["167.71.222.198"]
+ALLOWED_HOSTS = ["167.71.222.198", "127.0.0.1"]
 
 
 # Application definition
@@ -118,9 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # Default primary key field type
